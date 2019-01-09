7b28697002d2e604732126df59bdc8f68741e3f0
Mike Miller <39773669+mmillerlevels@users.noreply.github.com>
2019-01-08 22:28:15 -0700

    Update licenseChecker.sh

--- licenseChecker.sh
+++ licenseChecker.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
-#Get date within license
+#Get date within license, convert to epoch, get one week out
 licenseDate=$(grep '^Expiration' license.lic | sed 's/^Expiration=//')
 licenseDateUnix=$(date -d $licenseDate +%s)
-todayDate=$(date +%s)
+myDate=$(date -d "+7 days" +%s)
 
-if [ $todayDate -ge $licenseDateUnix ];
+#Is the license going to be expiring within a week?
+if [ $myDate -ge $licenseDateUnix ];
 then
 	echo Fail
-	echo 'Your ReachEngine license has expired! Please reach out to support@Customer.com!' | mail -s "License Alert" -r "reachengine@customerName.com" mmiller@levelsbeyond.com
+	echo Your license is about to expire soon, please Reach out for continued uptime!
+	#Need to tailor the following line to the particular customer's email conf (Postfix?)
+	#echo 'Your ReachEngine license has expired! Please reach out to support@Customer.com!' | mail -s "License Alert" -r "reachengine@customerName.com" mmiller@levelsbeyond.com
 fi
