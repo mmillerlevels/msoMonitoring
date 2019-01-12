19aa50ec7c5b25ea3765b942bf14efaf04629808
Mike Miller <39773669+mmillerlevels@users.noreply.github.com>
2019-01-11 23:50:37 -0700

    Update licenseChecker.sh

--- licenseChecker.sh
+++ licenseChecker.sh
@@ -11,8 +11,13 @@ myDate=$(date -d "+7 days" +%s)
 #Is the license going to be expiring within a week?
 if [ $myDate -ge $licenseDateUnix ];
 then
-	echo Fail
+	#Comment out the folliwng echo if using as a cron
+	#you only want the mailing piece.
+	#These are for the msoCheck
 	echo Your license is about to expire soon, please Reach out for continued uptime!
 	#Need to tailor the following line to the particular customer's email conf (Postfix?)
 	#echo 'Your ReachEngine license has expired! Please reach out to support@Customer.com!' | mail -s "License Alert" -r "reachengine@customerName.com" mmiller@levelsbeyond.com
+else
+	#Comment out below if using as a cron
+	echo Your license is good!
 fi
