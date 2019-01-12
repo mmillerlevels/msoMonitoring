896c65b5fcafcbf41fff907f1e582f011c06c7e5
Mike Miller <39773669+mmillerlevels@users.noreply.github.com>
2019-01-11 23:34:54 -0700

    Create properProps.pm

---
+++ properProps.pm
@@ -0,0 +1,24 @@
+#!/usr/bin/perl -w
+##2018 LevelsBeyond
+##Mike Miller @mmiller
+
+package properProps;
+use strict;
+use Tie::File;
+
+sub noMoMongo {
+	my $fileName   = 'myProps.prop';
+	my $searchStr  = 'mongo.enabled=true';
+	my $replaceStr = 'mongo.enabled=false';
+
+	tie my @fcont, "Tie::File", $fileName;
+
+	my $count;
+	for (@fcont) {
+    		$count++ if s/\Q$searchStr/$replaceStr/;
+	}
+
+	push @fcont, $replaceStr if ! $count;
+	print "Confirmed Mogno is no 'mo!\n";
+}
+1;
