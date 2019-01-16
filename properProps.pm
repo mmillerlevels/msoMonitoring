a64d99116e930a4cec871419472dba4c0fdcbcd8
Mike Miller <39773669+mmillerlevels@users.noreply.github.com>
2019-01-15 22:21:06 -0700

    Update properProps.pm

--- properProps.pm
+++ properProps.pm
@@ -9,6 +9,8 @@ use Tie::File;
 our $DEBUG;
 *DEBUG = \$main::DEBUG;
 
+our $props = 'myProps.prop';
+
 sub noMoMongo {
 	my $fileName   = 'myProps.prop';
 	my $searchStr  = 'mongo.enabled=true';
@@ -16,12 +18,25 @@ sub noMoMongo {
 
 	tie my @fcont, "Tie::File", $fileName;
 
-	my $count;
 	for (@fcont) {
-    		$count++ if s/\Q$searchStr/$replaceStr/;
+    		s/\Q$searchStr/$replaceStr/;
 	}
 
-	push @fcont, $replaceStr if ! $count;
+	my %exp = map { $_ => 1 } @fcont;
+	if(!exists($exp{$replaceStr})) {
+		push @fcont, $replaceStr if $count != '1';
+	}
 	print "Confirmed Mogno is no 'mo!\n" if $DEBUG;
 }
+
+sub vantageHost {
+	my $line = `grep \'\^vantage.host\=\' $props |  sed \'s\/\^vantage.hostn=\/\/\'`;
+	if ($line =~ /\b(?:(?:2(?:[0-4][0-9]|5[0-5])|[0-1]?[0-9]?[0-9])\.){3}(?:(?:2([0-4][0-9]|5[0-5])|[0-1]?[0-9]?[0-9]))\b/) {
+		print "You need a host name for your Vantage property!\n";
+	}
+	else {
+            print "Your vantage.host prop looks good\n" if $DEBUG;
+	}
+}
+
 1;
